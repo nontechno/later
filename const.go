2a26a0093b154b5d1f31cdf1eb59448ac5bdd29f
nontechno <github@nontechno.com>
2024-04-12 09:52:03 -0700

    v0

---
+++ const.go
@@ -0,0 +1,18 @@
+// Copyright 2024 The NonTechno Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package later
+
+const (
+	ReportFunc    = "report"
+	WarningFunc   = "warning"
+	ErrorFunc     = "error"
+	TerminateFunc = "terminate"
+
+	maxDepth        = 12
+	onErrorExitCode = 100
+	packageName     = "later"
+
+	separator = "\u0000"
+)
